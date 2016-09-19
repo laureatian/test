ef2b78a8ee86d1cce5422a0ea73a9dcd32723628
laureatian <tiantian.wang@intel.com>
2016-09-19 14:58:13 +0800

    commit ts1.c

---
+++ ts1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"ts2.c"
+int main()
+{
+int i;
+long result=0;
+for(i=0;i<100;i++)
+{
+result+=i;
+}
+
+printf("result[1-100]=%ld /n",result);
+printf("result[1-250]=%d /n",func(250));
+return 0;
+}
